fd308cf1547c594262ade381ceb2c6ea4b733714
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-05-31 10:18:24 -0400

    reveal spoiler

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
-function myCode() {
-$(".warning").hide().show("slow");
-}
+//prevent spoiler from being seen
+//Solution: hide spoiler
+//Solution: Reveal through user interaction
 
-myCode();
+//1. hide spoiler
+$(".spoiler span").hide();
+//2. add a button
+$(".spoiler").append("<button>Reveal Spoiler</button>");
+//3. When button is pressed
+	//3.1 show spoiler
+	$("button").click(function() {
+		$(".spoiler span").show();
+		$(this).remove();
+	});
+	//3.2 get rid of button
