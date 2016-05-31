8cebc2c7dfbdd35ce3e138e5a003a57c57c9db0a
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-05-31 11:42:12 -0400

    begin mobile dropdown

--- js/app.js
+++ js/app.js
@@ -1,39 +1,22 @@
-//Problem- when clicking on image, goes to dead end
-//Solution: create an overlay with large image - lightbox
+var $select = $("<select></select>");
+$("#menu").append($select);
 
-//2. Add overlay
-var $overlay = $('<div id="overlay"></div>');
-var $image = $('<img>');
-var $caption = $('<p></p>');
-
-$overlay.append($image);
-$overlay.append($caption);
-
-$("body").append($overlay);
-	//2.1 An Image
-	//2.2 A Caption
-
-
-//1. Capture the click event on a link to an image
-$("#imageGallery a").click(function( event) {
-	event.preventDefault();
-	
-	var imageLocation = $(this).attr("href");
-	$image.attr("src", imageLocation);
-
-
-	$overlay.show();
-
-	var $captionText = $(this).children("img").attr("alt");
-	$caption.text($captionText);
-
+$("#menu a").each(function() {
+  var $anchor = $(this);
+  var $option = $("<option></option>");
+  
+  if ($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  };
+  
+  $option.val($anchor.attr("href"));
+  
+  $option.text($anchor.text());
+  
+  $select.append($option);
 });
-	//1.1 show the overlay
-	//1.2 Update overlay with the image in the link
-	//1.3 Get child's alt attribute and set caption
 
-//3. When overlay is clicked
-	//3.1 hide overlay
-$overlay.click(function() {
-	$overlay.hide();
+
+$select.change(function(){
+  window.location = $select.val(); 
 });
