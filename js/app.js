5a6577a6871628614cdbe3f309eba8cb81508057
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-05-31 11:34:13 -0400

    complete lightbox

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@
 //2. Add overlay
 var $overlay = $('<div id="overlay"></div>');
 var $image = $('<img>');
-
+var $caption = $('<p></p>');
 
 $overlay.append($image);
-
+$overlay.append($caption);
 
 $("body").append($overlay);
 	//2.1 An Image
@@ -23,6 +23,9 @@ $("#imageGallery a").click(function( event) {
 
 
 	$overlay.show();
+
+	var $captionText = $(this).children("img").attr("alt");
+	$caption.text($captionText);
 
 });
 	//1.1 show the overlay
